6b99cb00fc04b57b05bbcdd24c93f6bf4ebc54ab
DilanOjeda <dilanangel12@gmail.com>
2022-03-03 11:55:34 -0400

    Add database name and volumes

--- container_creator.sh
+++ container_creator.sh
@@ -7,23 +7,25 @@ sudo sysctl -w vm.max_map_count=524288
 docker volume create sonarqube_data
 docker volume create sonarqube_extensions
 docker volume create sonarqube_logs
+docker volume create postgresql
+docker volume create postgresql_data
+docker volume create jenkins_home
 
 docker network create atnet
 
-mkdir ~/postgresql && mkdir ~/postgresl_data
-
-docker run -d --name sonardb \
---network atnet -p 5432:5432 \
+docker run -d --name postgresql \
+--network atnet \
 --restart always \
 -e POSTGRES_USER=sonar \
 -e POSTGRES_PASSWORD=sonaradmin \
+-e POSTGRES_DB=sonarqubedb \
 -v postgresql:/var/lib/postgresql \
 -v postgresql_data:/var/lib/postgresql/data \
 postgres:12.1-alpine
 
 docker run -d --name sonarqube \
 --network atnet -p 9000:9000 \
--e SONARQUBE_JDBC_URL=jdbc:postgresql://sonardb:5432/sonar \
+-e SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql:5432/sonarqubedb \
 -e SONARQUBE_JDBC_USERNAME=sonar \
 -e SONARQUBE_JDBC_PASSWORD=sonaradmin \
 -v sonarqube_data:/opt/sonarqube/data \
@@ -31,7 +33,7 @@ docker run -d --name sonarqube \
 -v sonarqube_logs:/opt/sonarqube/logs \
 sonarqube:8.9.0-community
 
-docker run -d --name sonarjenkins \
+docker run -d --name jenkins \
 --network atnet -p 8080:8080 -p 50000:50000 \
 -v jenkins_home:/var/jenkins_home \
 jenkins/jenkins:lts-jdk11
